41ae59fc4283ed2b99e4eb98dcdc62e60e9e9a8c
hipass7 <64785177+hipass7@users.noreply.github.com>
2022-03-31 21:06:00 +0900

    Update 1065.py

--- 220323/1065.py
+++ 220323/1065.py
@@ -2,13 +2,13 @@ num = int(input())
 cnt = 0
 for i in range(1, num + 1):
     i = str(i)
-    if len(i) < 3:
+    if len(i) < 3: # 두자리수의 경우
         cnt += 1
-    else:
+    else: # 세자리수의 경우
         for j in range(len(i) - 1):
-            n = int(i[j + 1]) - int(i[j])
+            n = int(i[j + 1]) - int(i[j]) # 첫 자리와 두 번째 자리 비교 후 저장
             try:
-                if int(i[j + 1]) + n == int(i[j+2]):
+                if int(i[j + 1]) + n == int(i[j + 2]): # 두 번째 자리와 세 번째 자리
                     continue
                 else:
                     break
