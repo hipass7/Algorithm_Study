67184963be4c5ae2c1c552d1a537569046ad4b81
hipass7 <64785177+hipass7@users.noreply.github.com>
2022-04-18 00:47:54 +0900

    Update 13458.py

--- 220418/13458.py
+++ 220418/13458.py
@@ -7,9 +7,15 @@ a, b = map(int, input().split())
 cnt = 0
 for i in range(num):
     people[i] -= a
-    cnt += 1
-    while people[i] > 0:
-        people[i] -= b
+    if people[i] < 0:
         cnt += 1
+        continue
+    else:
+        div = people[i] // b
+        th = people[i] % b
+        cnt += (div + 1)
+        while th > 0:
+            th -= b
+            cnt += 1
 
 print(cnt)
