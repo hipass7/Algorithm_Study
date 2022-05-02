39c9b72c71bbee61c21f34a71d970bf5bb7848cd
hipass7 <64785177+hipass7@users.noreply.github.com>
2022-05-02 23:01:42 +0900

    Update 바이러스전파BFS.py

--- 1차 SW 역량 테스트 대비 (220501)/바이러스전파BFS.py
+++ 1차 SW 역량 테스트 대비 (220501)/바이러스전파BFS.py
@@ -1,23 +1,22 @@
+from collections import deque
+
 n = int(input())
 k = int(input())
+graph = [[] for _ in range(n)]
 
-graph = [[0] * n for _ in range(n)]
-
-for i in range(k):
+for _ in range(k):
     a, b = map(int, input().split())
-    graph[a - 1][b - 1] = True
-    graph[b - 1][a - 1] = True
+    graph[a - 1].append(b - 1)
+    graph[b - 1].append(a - 1)
 
-queue = [0]
+queue = deque([0])
+
 visited = [0]
-result = []
-
 while queue:
-    start = queue.pop(0)
-    for i in range(n):
-        if graph[start][i]:
-            if i not in visited:
-                queue.append(i)
-                visited.append(i)
+    pt = queue.popleft()
+    for i in graph[pt]:
+        if i not in visited:
+            visited.append(i)
+            queue.append(i)
 
 print(len(visited) - 1)
