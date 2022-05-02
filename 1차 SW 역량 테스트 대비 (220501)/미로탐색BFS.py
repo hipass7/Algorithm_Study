a0d6e54fd3c0fe6502f5e9b149acebf304a16c49
hipass7 <64785177+hipass7@users.noreply.github.com>
2022-05-02 23:01:40 +0900

    Update 미로탐색BFS.py

--- 1차 SW 역량 테스트 대비 (220501)/미로탐색BFS.py
+++ 1차 SW 역량 테스트 대비 (220501)/미로탐색BFS.py
@@ -1,30 +1,23 @@
+from collections import deque
+
 n, m = map(int, input().split())
 
-graph = [list(map(int, input())) for _ in range(n)]
+graph = [list(map(int,input())) for _ in range(n)]
 
-def bfs(x, y):
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1] # 상 하 좌 우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-    queue = []
-    queue.append((x, y))
+queue = deque()
+queue.append((0,0))
 
-    while queue:
-        x, y = queue.pop(0)
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+while queue:
+    x, y = queue.popleft()
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if nx < 0 or nx > (n - 1) or ny < 0 or ny > (m - 1):
+            continue
+        if graph[nx][ny] == 1:
+            graph[nx][ny] = graph[x][y] + 1
+            queue.append((nx, ny))
 
-            if nx < 0 or nx > (n-1) or ny < 0 or ny > (m-1):
-                continue
-
-            if graph[nx][ny] == 0:
-                continue
-
-            if graph[nx][ny] == 1:
-                graph[nx][ny] += graph[x][y]
-                queue.append((nx, ny))
-
-    return graph[n - 1][m - 1]
-
-print(bfs(0, 0))
+print(graph[n - 1][m - 1])
