3ea75b9f94563b86aeefc69b7476ff82c9261bad
hipass7 <64785177+hipass7@users.noreply.github.com>
2022-05-02 23:01:37 +0900

    Update 1D최단거리BFS.py

--- 1차 SW 역량 테스트 대비 (220501)/1D최단거리BFS.py
+++ 1차 SW 역량 테스트 대비 (220501)/1D최단거리BFS.py
@@ -2,20 +2,16 @@ from collections import deque
 
 n, k = map(int, input().split())
 
-field = [0] * 100001
+graph = [0] * 100001
 
-def bfs():
-    queue = deque([])
-    queue.append(n)
-    while queue:
-        x = queue.popleft()
-        if x == k:
-            print(field[x])
-            break
+queue = deque([n])
 
-        for i in (x-1, x+1, 2 * x):
-            if 0 <= i <= 100000 and not field[i]:
-                field[i] = field[x] + 1
-                queue.append(i)
-
-bfs()
+while True:
+    n = queue.popleft()
+    if n == k:
+        print(graph[n])
+        break
+    for i in (n-1, n+1, 2 * n):
+        if 0 <= i < 100001 and graph[i] == 0:
+            queue.append(i)
+            graph[i] = graph[n] + 1
