b301a1e0db4f494e5b918cdde831b739e9fec2e5
hipass7 <64785177+hipass7@users.noreply.github.com>
2022-05-14 00:17:16 +0900

    Update 13458.py

--- 220418/13458.py
+++ 220418/13458.py
@@ -1,21 +1,44 @@
-num = int(input())
+import unittest
+from unittest.mock import Mock
 
-people = list(map(int, input().split()))
+mock = Mock()
 
-a, b = map(int, input().split())
+class prob(object):
+    def __init__(self, num, people, a, b):
+        self.num = num
+        self.people = people
+        self.a = a
+        self.b = b
+        self.cnt = 0
 
-cnt = 0
-for i in range(num):
-    people[i] -= a
-    if people[i] < 0:
-        cnt += 1
-        continue
-    else:
-        div = people[i] // b
-        th = people[i] % b
-        cnt += (div + 1)
-        while th > 0:
-            th -= b
-            cnt += 1
+    def result(self):
+        self.cnt = 0
+        for i in range(self.num):
+            self.people[i] -= self.a
+            if self.people[i] < 0:
+                self.cnt += 1
+                continue
+            else:
+                div = self.people[i] // self.b
+                th = self.people[i] % self.b
+                self.cnt += (div + 1)
+                while th > 0:
+                    th -= self.b
+                    self.cnt += 1
+        mock.return_value = self.cnt
+        return mock()
 
-print(cnt)
+class Test(unittest.TestCase):
+    def test_1(self):
+        t = prob(1, [1], 1, 1)
+        self.assertEqual(1, t.result())
+
+    def test_2(self):
+        t = prob(3, [3, 4, 5], 2, 2)
+        self.assertEqual(7, t.result()) # 정답은 원래 7
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
+
