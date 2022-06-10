3b4d58f2b91a5db8fb9f66980496db78d8735de3
hipass7 <64785177+hipass7@users.noreply.github.com>
2022-06-10 23:36:07 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,23 @@
-from itertools import product
+n = int(input())
+k = int(input())
 
-print(type(int(list(product('0123', repeat=5))[0][0])))
+graph = [[0] * n for _ in range(n)]
+
+for i in range(k):
+    a, b = map(int, input().split())
+    graph[a - 1][b - 1] = True
+    graph[b - 1][a - 1] = True
+
+queue = [0]
+visited = [0]
+result = []
+
+while queue:
+    start = queue.pop(0)
+    for i in range(n):
+        if graph[start][i]:
+            if i not in visited:
+                queue.append(i)
+                visited.append(i)
+
+print(len(visited) - 1)
