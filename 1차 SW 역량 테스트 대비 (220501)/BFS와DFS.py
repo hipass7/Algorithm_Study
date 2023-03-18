527ac1f0d94fd34f4c8d179f7dd60baa240b01ae
hipass7 <64785177+hipass7@users.noreply.github.com>
2023-03-18 23:36:05 +0900

    230318
    230318

--- 1차 SW 역량 테스트 대비 (220501)/BFS와DFS.py
+++ 1차 SW 역량 테스트 대비 (220501)/BFS와DFS.py
@@ -1,49 +1,29 @@
 from collections import deque
 
 n, m, v = map(int, input().split())
-graph = [[] for _ in range(n)]
-v = v - 1
-for _ in range(m):
+graph = [[0] * (n + 1) for _ in range(n + 1)]
+
+for i in range(m):
     a, b = map(int, input().split())
-    graph[a - 1].append(b - 1)
-    graph[b - 1].append(a - 1)
+    graph[a][b] = 1
+    graph[b][a] = 1
 
-for _ in range(n):
-    graph[_].sort()
-
-def bfs(v):
-    visited = [0] * n
-    result = []
-    queue = deque()
-    queue.append(v)
-    result.append(v)
-    visited[v] = True
-    while queue:
-        pt = queue.popleft()
-        for i in graph[pt]:
-            if not visited[i]:
-                visited[i] = True
-                queue.append(i)
-                result.append(i)
-    return result
-
-visited = [0] * n
+# BFS
+visited = [False] * (n + 1)
 result = []
+queue = deque()
+queue.append(v)
+result.append(v)
+visited[v] = True
+while queue:
+    pt = queue.popleft()
+    for i in range(1, n+ 1):
+        if graph[pt][i] == 1 and not visited[i]:
+            visited[i] = True
+            queue.append(i)
+            result.append(i)
 
-def dfs(v):
-    visited[v] = True
-    result.append(v)
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(i)
+print(graph)
+print(result)
 
-
-dfs(v)
-for i in range(len(result)):
-     result[i] += 1
-print(*result, sep=' ')
-result = bfs(v)
-for i in range(len(result)):
-     result[i] += 1
-print(*result, sep=' ')
 
